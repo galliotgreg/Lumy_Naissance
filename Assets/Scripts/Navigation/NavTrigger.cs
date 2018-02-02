9aefcc42ba01e605b4c462facb6e91fe84d884bf
J-Ponzo <jeremy.ponzo@gmail.com>
2018-02-02 13:35:24 +0100

    commit empty scenes

---
+++ Assets/Scripts/Navigation/NavTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+            NavigationManager.instance.SwapScenes("TitleScene", "CampaignScene", 2, Vector3.zero);
+        }
+	}
+}
---
+++ Assets/Scripts/Navigation/NavigationManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationManager : MonoBehaviour {
+    /// <summary>
+    /// The static instance of the Singleton for external access
+    /// </summary>
+    public static NavigationManager instance = null;
+
+    /// <summary>
+    /// Enforce Singleton properties
+    /// </summary>
+    void Awake()
+    {
+        //Check if instance already exists and set it to this if not
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        //Enforce the unicity of the Singleton
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    [SerializeField]
+    private string[] initSceneLayers;
+    [SerializeField]
+    private Camera camera;
+
+
+    public float zoomEndDistance = 0.0f;
+    public float zoomStep = 0.1f;
+    public float fadeStep = 0.05f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(InitSceneLayers());
+    }
+
+    IEnumerator InitSceneLayers()
+    {
+        AsyncOperation loadLayer1 = SceneManager.LoadSceneAsync(initSceneLayers[1], LoadSceneMode.Additive);
+        AsyncOperation loadLayer2 = SceneManager.LoadSceneAsync(initSceneLayers[2], LoadSceneMode.Additive);
+        while (!loadLayer1.isDone)
+        {
+            yield return null;
+        }
+        while (!loadLayer2.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void SwapScenes(string curScene, string nextScene, int layer, Vector3 sightPoint)
+    {
+        StartCoroutine(ZoomIn(curScene, nextScene, layer, sightPoint));
+    }
+
+    IEnumerator ZoomIn(string curScene, string nextScene, int layer, Vector3 sightPoint)
+    {
+        GameObject root = SceneManager.GetSceneByName(curScene).GetRootGameObjects()[0];
+
+
+        // Zoomer sur le bouton
+        GameObject canvas = GameObject.Find("WorldCanvas");
+        Vector3 dir = (root.transform.position - camera.transform.position).normalized;
+        while (Vector3.Dot(dir, root.transform.position - camera.transform.position) > zoomEndDistance)
+        {
+            Vector3 towards = Vector3.MoveTowards(
+                root.transform.position,
+                camera.transform.position + root.transform.position - sightPoint,
+                zoomStep);
+            root.transform.position = towards;
+            //GameObject.Find("WorldCanvas").GetComponent<CanvasGroup>().alpha -= fadeStep;
+            yield return true;
+        }
+
+        // Attendre la fin du déchargement de la scène initiale
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(curScene);
+        while (!unload.isDone)
+        {
+            Debug.Log("unloading");
+            yield return null;
+        }
+        Debug.Log("unloaded");
+
+        // Attendre la fin du chargement de la scène de destination
+        AsyncOperation load = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+        while (!load.isDone)
+        {
+            Debug.Log("loading");
+            yield return null;
+        }
+        Debug.Log("loaded");
+
+        // Faire apparaître le canvas en fondu
+        //GameObject.Find("WorldCanvas").GetComponent<CanvasGroup>().alpha = 0f;
+        //float alpha = GameObject.Find("WorldCanvas").GetComponent<CanvasGroup>().alpha;
+        //while (alpha < 1.0f)
+        //{
+        //    alpha = GameObject.Find("WorldCanvas").GetComponent<CanvasGroup>().alpha;
+        //    GameObject.Find("WorldCanvas").GetComponent<CanvasGroup>().alpha += fadeStep;
+        //    yield return true;
+        //}
+
+        // Arrêter la coroutine de transition
+        StopCoroutine(ZoomIn(curScene, nextScene, layer, sightPoint));
+        yield return true;
+    }
+}
