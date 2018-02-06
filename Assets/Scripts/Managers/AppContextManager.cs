2807f0d8cf42aa5505ad2ee5ac03b231020d6215
J-Ponzo <jeremy.ponzo@gmail.com>
2018-02-06 15:05:29 +0100

    AppContexxtManager : SwitchActiveCast, SwitchActiveSpecie, GetSepciesNames

--- Assets/Scripts/Managers/AppContextManager.cs
+++ Assets/Scripts/Managers/AppContextManager.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class AppContextManager : MonoBehaviour {
+public class AppContextManager : MonoBehaviour
+{
     /// <summary>
     /// The static instance of the Singleton for external access
     /// </summary>
@@ -28,91 +31,212 @@ public class AppContextManager : MonoBehaviour {
     }
 
     [SerializeField]
-    private string curSpecieFolderName;
+    private string activeSpeciePath;
     [SerializeField]
-    private List<string> speciesFolderNames = new List<string>();
+    private string activeBehaviorPath;
+    [SerializeField]
+    private string[] speciesFolderNames;
+    [SerializeField]
+    private string[] castsFileNames;
 
     /// <summary>
-    /// The selected Swarm definition
+    /// Path to the folder hosting all available species
     /// </summary>
-    private Specie currentSpecie;
-
+    [SerializeField]
+    private string SPECIES_FOLDER_PATH = "Assets/Inputs/Species/";
     /// <summary>
-    /// The selected Cast definition
+    /// Casts files suffix
     /// </summary>
-    private Cast currentCast;
+    [SerializeField]
+    private string CAST_FILES_SUFFIX = "_behavior.csv";
 
-    public Specie CurrentSpecie
+    public string ActiveSpeciePath
     {
         get
         {
-            return currentSpecie;
+            return activeSpeciePath;
         }
 
         set
         {
-            currentSpecie = value;
+            activeSpeciePath = value;
         }
     }
 
-    public Cast CurrentCast
+    public string ActiveBehaviorPath
     {
         get
         {
-            return currentCast;
+            return activeBehaviorPath;
         }
 
         set
         {
-            currentCast = value;
+            activeBehaviorPath = value;
         }
     }
 
-    public List<string> SpeciesFolderNames
+    public void SwitchActiveCast(string castName)
     {
-        get
+        //Check if specie existe
+        bool found = false;
+        foreach (string name in castsFileNames)
         {
-            return speciesFolderNames;
+            if (name == castName)
+            {
+                found = true;
+                break;
+            }
         }
-    
-        set
+        if (!found)
         {
-            speciesFolderNames = value;
+            Debug.LogError(castName + " Does not exists");
         }
+
+        //Set Specie path
+        activeBehaviorPath = GetPathFromCastName(castName);
     }
 
-    public string CurSpecieFolderName
+    public void SwitchActiveSpecie(string specieName)
     {
-        get
+        //Check if specie existe
+        bool found = false;
+        foreach (string name in speciesFolderNames)
         {
-            return curSpecieFolderName;
+            if (name == specieName)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Debug.LogError(specieName + " Does not exists");
         }
 
-        set
-        {
-            curSpecieFolderName = value;
-        }
+        //Set Specie path
+        activeSpeciePath = GetPathFromSpecieName(specieName);
+
+        //Set default cast
+        UpdateCastsFilesNames();
+        SetupDefaultActiveCast();
     }
 
-    public void SwitchSpecie(string specieName)
+    public string[] GetSpeciesFolderNames()
     {
-        //Read file
-        StreamReader reader = new StreamReader(curSpecieFolderName);
-        List<string> lines = new List<string>();
-        while (reader.Peek() >= 0)
-        {
-            lines.Add(reader.ReadLine());
-        }
+        return speciesFolderNames;
+    }
 
-        SpecieParser parser = new SpecieParser();
-        currentSpecie = parser.Parse(lines);
+    public string[] GetCastFileNames()
+    {
+        return castsFileNames;
+    }
+
+    public void CreateSpecie(string specieName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ForkCast(string specieName)
+    {
+        throw new NotImplementedException();
     }
 
     void Start()
     {
-        //Use player preferences
+        //Retrieve Specie Folders Names
+        UpdateSpeciesFoldersNames();
+        SetupDefaultActiveSpecie();
 
+        //Retrieve Cast Files Names
+        UpdateCastsFilesNames();
+        SetupDefaultActiveCast();
+    }
+
+    private void SetupDefaultActiveCast()
+    {
+        //Setup active cast
+        //Check player preferences
+        //TODO
         //If player preferences are empty
+        activeBehaviorPath = GetPathFromCastName(castsFileNames[0]);
+    }
 
+    private void SetupDefaultActiveSpecie()
+    {
+        //Setup active specie
+        //Check player preferences
+        //TODO
+        //If player preferences are empty
+        activeSpeciePath = GetPathFromSpecieName(speciesFolderNames[0]);
+    }
+
+    private string GetPathFromCastName(string name)
+    {
+        return SPECIES_FOLDER_PATH + name + CAST_FILES_SUFFIX + "/";
+    }
+
+    private string GetPathFromSpecieName(string name)
+    {
+        return SPECIES_FOLDER_PATH + name + "/";
+    }
+
+    private void UpdateCastsFilesNames()
+    {
+        //Retrieve all directory content
+        DirectoryInfo info = new DirectoryInfo(activeSpeciePath);
+        FileInfo[] filesInfo = info.GetFiles();
+        IList<string> castFileNamesList = new List<string>();
+
+        //Filter behavior files
+        for (int i = 0; i < filesInfo.Length; i++)
+        {
+            if (IsCastFile(filesInfo[i].Name))
+            {
+                castFileNamesList.Add(filesInfo[i].Name);
+            }
+        }
+
+        //Set attribute
+        castsFileNames = new string[castFileNamesList.Count];
+        for (int i = 0; i < castFileNamesList.Count; i++)
+        {
+            string name = castFileNamesList[i].Replace(CAST_FILES_SUFFIX, "");
+            castsFileNames[i] = name;
+        }
+    }
+
+    private bool IsCastFile(string name)
+    {
+        //Check if is behavior file
+        string pat = @".+" + CAST_FILES_SUFFIX;
+        Regex r = new Regex(pat);
+        Match m = r.Match(name);
+        if (!(m.Length > 0))
+        {
+            return false;
+        }
+
+        //Check if is meta file
+        pat = @".+meta";
+        r = new Regex(pat);
+        m = r.Match(name);
+        if (m.Length > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void UpdateSpeciesFoldersNames()
+    {
+        DirectoryInfo info = new DirectoryInfo(SPECIES_FOLDER_PATH);
+        DirectoryInfo[] dirsInfo = info.GetDirectories();
+        speciesFolderNames = new string[dirsInfo.Length];
+        for (int i = 0; i < dirsInfo.Length; i++)
+        {
+            speciesFolderNames[i] = dirsInfo[i].Name;
+        }
     }
 }
