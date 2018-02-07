9fcf9c3a77d2d03d51e7efdf3939a9dee9a22514
kbot22 <eric.boucher22@outlook.fr>
2018-02-07 16:26:32 +0100

    Creation des scenes de menu
    Premiere creation des scenes necessaires pour les menus : Menu principal, Option, Affrontement, Partie Personalisé, Vidéo, Les Editeurs (MC, Lumy, Castes), Menu In Game et Collection

---
+++ Assets/Scripts/View/ViewSubMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewSubMenu : MonoBehaviour {
+
+    public GameObject subMenu;
+    public GameObject subMenuBis;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(SwitchMenu);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SwitchMenu()
+    {
+        subMenu.active = !subMenu.active;
+
+        if (subMenuBis.active) subMenuBis.active = !subMenuBis.active;
+    }
+}
