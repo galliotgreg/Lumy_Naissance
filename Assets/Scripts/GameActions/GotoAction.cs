6fa4da293ae38f5435736601ded1206e1cf520cb
Unknown <edwynluis@yahoo.com.br>
2018-02-08 18:20:26 +0100

    GotoAction : new implementation

--- Assets/Scripts/GameActions/GotoAction.cs
+++ Assets/Scripts/GameActions/GotoAction.cs
@@ -32,13 +32,28 @@ public class GotoAction : GameAction {
 		// Setting next target
 		if (path.Length > 0)
 		{
-			agentAttr.TrgPos = path[0];
-		}
+			if (path.Length == 1) {
+				agentAttr.TrgPos = path [0];
+			} else {
+				// selectNext Point
+				int closestIndex = indexClosest( agentAttr.transform.position, path );
 
-		// Use Unity A* to move
-		UnityEngine.AI.NavMeshAgent movingAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-		agentAttr.transform.position = moveTo( agentAttr, movingAgent );
-		//agentAttr.CurPos = new Vector2( agentAttr.transform.position.x, agentAttr.transform.position.z );
+				// On a point
+				if (isClose (agentAttr.transform.position, path [closestIndex])) {
+					// set next target (or the last point, if the index is the last)
+					agentAttr.TrgPos = (closestIndex == path.Length-1?path [closestIndex]:path [closestIndex+1]);
+				} else {
+					// On an intersection
+					int edge = getEdge( agentAttr.transform.position, closestIndex, path );
+					agentAttr.TrgPos = path [closestIndex + edge];
+				}
+			}
+
+			// Use Unity A* to move
+			UnityEngine.AI.NavMeshAgent movingAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+			agentAttr.transform.position = moveTo( agentAttr, movingAgent );
+			//agentAttr.CurPos = new Vector2( agentAttr.transform.position.x, agentAttr.transform.position.z );
+		}
 	}
 
 	#endregion
@@ -54,7 +69,7 @@ public class GotoAction : GameAction {
 
 			UnityEngine.AI.NavMeshPath path = new UnityEngine.AI.NavMeshPath();
 			navMeshAgent.CalculatePath( destination, path );
-            Debug.Log(destination); 
+
 			// move towards next corner
 			if (path.corners.Length > 0) {
     			
@@ -62,5 +77,44 @@ public class GotoAction : GameAction {
 			}
 		}
 		return agentAttr.transform.position;
+	}
+
+	private int indexClosest( Vector3 pos, Vector3[] _path ){
+		int resultIndex = 0;
+		float resultDistance = Vector3.Distance (pos, _path [resultIndex]);
+		for (int i = 1; i < _path.Length; i++) {
+			float auxDistance = Vector3.Distance (pos, _path [i]);
+			if ( auxDistance < resultDistance ) {
+				resultIndex = i;
+				resultDistance = auxDistance;
+			}
+		}
+
+		return resultIndex;
+	}
+
+	private bool isClose( Vector3 pos, Vector3 point ){
+		return Vector3.Distance (pos, point) > 0.01f;
+	}
+
+	// -1 edge towards the previous point
+	// 1 edge towards the next point
+	// 0 not edge
+	private int getEdge( Vector3 pos, int index, Vector3[] _path ){
+		if (index > 0) {
+			if (isEdge (pos, _path [index-1], _path [index])) {
+				return -1;
+			}
+		}
+		if (index < _path.Length-1) {
+			if (isEdge (pos, _path [index], _path [index+1])) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	private bool isEdge( Vector3 pos, Vector3 a, Vector3 b ){
+		return Vector3.Dot( (a-b).normalized, (a-pos).normalized ) > 0.99f ;
 	}
 }
---
+++ Assets/Tests/Behaviors/CreateAgentTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateAgentTest : MonoBehaviour {
+
+	[SerializeField]
+	string castName;
+
+	[SerializeField]
+	PlayerAuthority authority = PlayerAuthority.Player1;
+
+	[SerializeField]
+	Vector2 pos;
+
+	GameObject agent = null;
+	bool initialized = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (agent == null && !initialized) {
+			GameObject obj = GameManager.instance.GetUnitTemplate (authority, castName);
+			if (obj != null) {
+				agent = Instantiate (obj, pos, Quaternion.identity);
+				agent.SetActive (true);
+				HomeScript home = GameManager.instance.GetHome (authority);
+				home.addUnit (agent.GetComponent<AgentEntity> ());
+				agent.name = agent.GetComponent<AgentEntity> ().CastName + "-" + home.Authority.ToString();
+
+				initialized = true;
+			}
+		}
+	}
+}
