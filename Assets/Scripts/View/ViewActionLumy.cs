8285587502a35b307cf9b6cbcc6a6f9d1b6f59b1
kbot22 <eric.boucher22@outlook.fr>
2018-02-08 16:19:01 +0100

    Rangement du CastesScene
    Réparation du premier fail de GitHub, sauvegarde des modification apporté à la CastesScene

---
+++ Assets/Scripts/View/ViewActionLumy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ViewActionLumy : MonoBehaviour {
+
+    GameObject subMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(SwitchMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SwitchMenu()
+    {
+    /*    subMenu.SetActive(!subMenu.activeSelf);
+
+        foreach (Transform child in transform.parent.parent)
+        {
+            if (child.Find("PanelAction").activeSelf && child != transform.parent) child.gameObject.SetActive(!child.gameObject.activeSelf);
+        }*/
+    }
+}
