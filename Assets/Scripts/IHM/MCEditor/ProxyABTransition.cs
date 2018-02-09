97121110433e65bdd969b7d3dfd8e72ff8d5fa78
Narlugo <narutogoku2@hotmail.fr>
2018-02-09 13:54:05 +0100

    refacto and refresh of transition

--- Assets/Scripts/IHM/MCEditor/ProxyABTransition.cs
+++ Assets/Scripts/IHM/MCEditor/ProxyABTransition.cs
@@ -7,15 +7,40 @@ public class ProxyABTransition : MonoBehaviour {
     private Pin startPosition;
     private Pin endPosition;
 
+    public Pin StartPosition {
+        get {
+            return startPosition;
+        }
+
+        set {
+            startPosition = value;
+        }
+    }
+
+    public Pin EndPosition {
+        get {
+            return endPosition;
+        }
+
+        set {
+            endPosition = value;
+        }
+    }
+
     // Use this for initialization
     void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if(StartPosition != null && EndPosition != null) {
+
+            Vector3 posDepart = StartPosition.transform.position;
+            Vector3 posArrivee = EndPosition.transform.position;
+            GetComponent<LineRenderer>().SetPosition(0, posDepart);
+            GetComponent<LineRenderer>().SetPosition(1, posArrivee);
+        }
+    }
 
     public ProxyABTransition(ABTransition transition)
     {
