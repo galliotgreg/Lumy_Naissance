4b456a08d71b2cb89ad79412280e43d462727c35
kbot22 <eric.boucher22@outlook.fr>
2018-02-09 16:25:00 +0100

    Ajout de l'éditeur de Caste
    Les methodes sont operationnel pour changer de menu, (Attention seul UI est faite)

---
+++ Assets/Scripts/View/Close.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Close : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+        this.gameObject.GetComponent<Button>().onClick.AddListener(ClosePanel);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void ClosePanel()
+    {
+        transform.parent.gameObject.SetActive(!transform.parent.gameObject.activeSelf);
+    }
+}
