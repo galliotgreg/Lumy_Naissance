ce70f065ebebd532dd5c0df78364557f73834cf0
J-Ponzo <jeremy.ponzo@gmail.com>
2018-02-09 11:50:08 +0100

    Intégration du lumy du poc dans le lummy prefab

---
+++ Assets/Scripts/GameObject/LumyMotion/PhyBone.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhyBone : MonoBehaviour {
+    //UI Vars
+    [SerializeField]
+    private Vector2 origin;
+    [SerializeField]
+    private Vector2 end;
+    [SerializeField]
+    private float localTeta = 0f;
+    [SerializeField]
+    private float worldTeta = 0f;
+    [SerializeField]
+    private float lenght = 0.5f;
+    [SerializeField]
+    private PhyJoin baseJoin;
+    [SerializeField]
+    private PhyJoin headJoin;
+
+    [SerializeField]
+    private Vector2 IKTarget;
+
+    public PhyJoin BaseJoin
+    {
+        get
+        {
+            return baseJoin;
+        }
+
+        set
+        {
+            baseJoin = value;
+        }
+    }
+
+    public PhyJoin HeadJoin
+    {
+        get
+        {
+            return headJoin;
+        }
+
+        set
+        {
+            headJoin = value;
+        }
+    }
+
+    public float Lenght
+    {
+        get
+        {
+            return lenght;
+        }
+
+        set
+        {
+            lenght = value;
+        }
+    }
+
+    public float LocalTeta
+    {
+        get
+        {
+            return localTeta;
+        }
+
+        set
+        {
+            localTeta = value;
+        }
+    }
+
+    public float WorldTeta
+    {
+        get
+        {
+            return worldTeta;
+        }
+
+        set
+        {
+            worldTeta = value;
+        }
+    }
+
+    public Vector2 Origin
+    {
+        get
+        {
+            return origin;
+        }
+
+        set
+        {
+            origin = value;
+        }
+    }
+
+    public Vector2 End
+    {
+        get
+        {
+            return end;
+        }
+
+        set
+        {
+            end = value;
+        }
+    }
+
+    public void ComputeEnd()
+    {
+        Vector2 dir = new Vector2(
+            Lenght * Mathf.Cos(Mathf.Deg2Rad * WorldTeta),
+            Lenght * Mathf.Sin(Mathf.Deg2Rad * WorldTeta));
+        End = Origin + dir;
+    }
+}
---
+++ Assets/Scripts/GameObject/LumyMotion/PhyLimb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhyLimb {
+    private List<PhyLimb> childs = new List<PhyLimb>();
+    private List<PhyBone> bones = new List<PhyBone>();
+
+    public List<PhyLimb> Childs
+    {
+        get
+        {
+            return childs;
+        }
+
+        set
+        {
+            childs = value;
+        }
+    }
+
+    public List<PhyBone> Bones
+    {
+        get
+        {
+            return bones;
+        }
+
+        set
+        {
+            bones = value;
+        }
+    }
+}
