821d4a2994711c6b0fee4cc1f7ae4925cd5e51ed
Babagreen <thomasousselin0902@gmail.com>
2018-02-13 16:23:36 +0100

    Script d'ouverture de volet verticaux
    Ouverture des différent panel avec toujours 1 panael ouvert.
    Reste à faire: afficher les bouton du panel Node si il est ouvert

--- Assets/Scripts/IHM/Navigation/OpenVoletVertical.cs
+++ Assets/Scripts/IHM/Navigation/OpenVoletVertical.cs
@@ -8,6 +8,7 @@ public class OpenVoletVertical : MonoBehaviour {
 	private float headerHeight = 30;
 	[SerializeField]
 	private float panelHeight = 990;
+	private float windowHeight = 1080;
     GameObject panelNodeHeaderClick;
     GameObject panelActionHeaderClick;
     GameObject panelParamsHeaderClick;
@@ -66,42 +67,231 @@ public class OpenVoletVertical : MonoBehaviour {
 	void Update () {
 		
 	}
-	public void OpenCloseFirstPanel(){
+	public void OpenFirstPanel(){
+		GameObject btnCreer;
+		GameObject chkSynta;
+		GameObject chkAction;
 		float heightPanel = rtActions.rect.height;
 		float heightHeaderPanel = rtActionsHeader.rect.height;
-		bool isOpen = voletsIsOpen [0];
-		float pos;
+		bool isOpen = voletsIsOpen [1];
+		float recenterPos;
 		//Fermeture
-		if (isOpen == true) 
-		{
-			heightPanel = rtNode.rect.width / 36;
-			rtNode.sizeDelta = new Vector2 (rtNode.rect.width, heightPanel);
-			pos = rtNode.rect.position.y;
-			Debug.Log ("Position Y node: " + pos);
-			Debug.Log ("Position X node: " + rtNode.rect.position.x);
-			pos = rtNode.rect.height - (heightPanel+heightHeaderPanel*2)/100;
-			rtNode.SetPositionAndRotation (new Vector3(rtNode.rect.position.x,pos,0), Quaternion.Euler(0,0,0));
-			Debug.Log ("Position Y node2: " + pos);
-			Debug.Log ("Position X node2: " + rtNode.rect.position.x);
-		} 
-		//Ouverture
-		else 
-		{
-			heightPanel = rtActions.rect.width * 36;
-			rtNode.sizeDelta = new Vector2 (rtActions.rect.width, heightPanel);
-		}
+		heightPanel = headerHeight;
+		rtNode.sizeDelta = new Vector2 (rtNode.rect.width, heightPanel);
+		btnCreer = GameObject.Find("btn_Creer");
+		chkSynta = GameObject.Find ("chck_Syntaxique");
+		chkAction = GameObject.Find ("chck_Actions");
+		/*if (btnCreer.activeSelf == true) {
+			btnCreer.SetActive (true);
+			chkSynta.SetActive (true);
+			chkAction.SetActive (true);
+		} else {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}*/
 
+		//Deplacement du volet du dessus en haut de l'écran
+		float anchorPosition = ((3*headerHeight)+(panelHeight-headerHeight)+(headerHeight/2))-windowHeight;
+		float anchorX = rtNode.anchoredPosition.x;
+		float anchorY = rtNode.anchoredPosition.y;
+		//Ouverture du panel cliqué
+		anchorX = rtActions.anchoredPosition.x;
+		anchorY = rtActions.anchoredPosition.y;
+		anchorPosition =-(panelHeight/2);
+		rtNode.anchoredPosition = new Vector2 (rtActions.anchoredPosition.x, anchorPosition);
+		rtNode.sizeDelta = new Vector2(rtActions.rect.width, panelHeight);
+
+		//Fermeture des volet du dessous
+		//1er volet en dessous
+		anchorX =rtActions.anchoredPosition.x;
+		anchorY = rtActions.anchoredPosition.y;
+		anchorPosition = -(panelHeight + (headerHeight / 2));
+		rtActions.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtActions.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
+		//2er volmet en dessou
+		anchorX = rtParams.anchoredPosition.x;
+		anchorY = rtParams.anchoredPosition.y;
+		anchorPosition = -(panelHeight + (headerHeight / 2) +headerHeight);
+		rtParams.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtParams.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
+		//3eme volet en dessous
+		anchorX = rtOpe.anchoredPosition.x;
+		anchorY = rtOpe.anchoredPosition.y;
+		anchorPosition = -(panelHeight + (headerHeight / 2) +(2*headerHeight));
+		rtOpe.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtOpe.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
 
 	}
-	public void OpenCloseSecondPanel(){
-		
+	public void OpenSecondPanel(){
+		GameObject btnCreer;
+		GameObject chkSynta;
+		GameObject chkAction;
+		float heightPanel = rtActions.rect.height;
+		float heightHeaderPanel = rtActionsHeader.rect.height;
+		bool isOpen = voletsIsOpen [1];
+		float recenterPos;
+		//Fermeture
+		heightPanel = 30;//rtNode.rect.height / 36;
+		rtNode.sizeDelta = new Vector2 (rtNode.rect.width, heightPanel);
+		btnCreer = GameObject.Find("btn_Creer");
+		chkSynta = GameObject.Find ("chck_Syntaxique");
+		chkAction = GameObject.Find ("chck_Actions");
+
+		/*if (btnCreer.activeSelf == true) {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}else {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}*/
+
+		//Deplacement du volet du dessus en haut de l'écran
+		recenterPos = rtNode.rect.position.y;
+		float anchorX = rtNode.anchoredPosition.x;
+		float anchorY = rtNode.anchoredPosition.y;
+		float anchorPosition = ((3*headerHeight)+(panelHeight-headerHeight)+(headerHeight/2))-windowHeight;
+
+		rtNode.anchoredPosition= new Vector2 (rtNode.anchoredPosition.x, anchorPosition);
+		anchorX = rtNode.anchoredPosition.x;
+		anchorY = rtNode.anchoredPosition.y;
+
+		//Ouverture du panel cliqué
+		anchorX = rtActions.anchoredPosition.x;
+		anchorY = rtActions.anchoredPosition.y;
+		anchorPosition = ((2*headerHeight)+((panelHeight-(2*headerHeight))/2)+(headerHeight))-windowHeight;
+		rtActions.anchoredPosition = new Vector2 (rtActions.anchoredPosition.x, anchorPosition);
+		rtActions.sizeDelta = new Vector2(rtActions.rect.width, panelHeight);
+
+		//Fermeture des volet du dessous
+		//1er volet en dessou
+		anchorX = rtParams.anchoredPosition.x;
+		anchorY = rtParams.anchoredPosition.y;
+		anchorPosition = -(panelHeight + headerHeight) - headerHeight / 2;
+		rtParams.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtParams.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
+		//2eme volet en dessous
+		anchorX = rtOpe.anchoredPosition.x;
+		anchorY = rtOpe.anchoredPosition.y;
+		anchorPosition = -(panelHeight + (2*headerHeight)) - headerHeight / 2;
+		rtOpe.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtOpe.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
 	}
-	public void OpenCloseThirdPanel(){
-		
+	public void OpenThirdPanel(){
+		GameObject btnCreer;
+		GameObject chkSynta;
+		GameObject chkAction;
+		float heightPanel = rtActions.rect.height;
+		float heightHeaderPanel = rtActionsHeader.rect.height;
+		bool isOpen = voletsIsOpen [1];
+		float recenterPos;
+		//Fermeture
+		heightPanel = 30;//rtNode.rect.height / 36;
+		rtNode.sizeDelta = new Vector2 (rtNode.rect.width, heightPanel);
+		btnCreer = GameObject.Find("btn_Creer");
+		chkSynta = GameObject.Find ("chck_Syntaxique");
+		chkAction = GameObject.Find ("chck_Actions");
+
+		/*if (btnCreer.activeSelf == true) {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}else {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}*/
+
+		//Deplacement des volet du dessus en haut de l'écran
+		recenterPos = rtNode.rect.position.y;
+		float anchorX = rtNode.anchoredPosition.x;
+		float anchorY = rtNode.anchoredPosition.y;
+
+		//1er volet du dessus
+		float anchorPosition = ((3*headerHeight)+(panelHeight-headerHeight)+(headerHeight/2))-windowHeight;
+		rtNode.anchoredPosition= new Vector2 (rtNode.anchoredPosition.x, anchorPosition);
+		anchorX = rtNode.anchoredPosition.x;
+		anchorY = rtNode.anchoredPosition.y;
+
+		//2eme volet en dessu
+		anchorX = rtActions.anchoredPosition.x;
+		anchorY = rtActions.anchoredPosition.y;
+		anchorPosition = -((headerHeight/2) + headerHeight);
+		rtActions.anchoredPosition = new Vector2 (rtActions.anchoredPosition.x, anchorPosition);
+		rtActions.sizeDelta =new Vector2(rtActions.rect.width, headerHeight);
+
+		//Ouverture du panel cliqué
+		anchorX = rtParams.anchoredPosition.x;
+		anchorY = rtParams.anchoredPosition.y;
+		anchorPosition = -((panelHeight / 2) + 2*headerHeight);
+		rtParams.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtParams.sizeDelta = new Vector2(rtParams.rect.width, panelHeight);
+
+		//Fermeture du volet du dessous
+		anchorX = rtOpe.anchoredPosition.x;
+		anchorY = rtOpe.anchoredPosition.y;
+		anchorPosition = -(panelHeight + (2*headerHeight)) - headerHeight / 2;
+		rtOpe.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtOpe.sizeDelta =new Vector2(rtParams.rect.width, headerHeight);
 	}
-	public void OpenCloseFourthPanel(){
-		
+	public void OpenFourthPanel(){
+		GameObject btnCreer;
+		GameObject chkSynta;
+		GameObject chkAction;
+		float heightPanel = rtActions.rect.height;
+		float heightHeaderPanel = rtActionsHeader.rect.height;
+		bool isOpen = voletsIsOpen [1];
+		float recenterPos;
+		//Fermeture
+		heightPanel = 30;//rtNode.rect.height / 36;
+		rtNode.sizeDelta = new Vector2 (rtNode.rect.width, heightPanel);
+		btnCreer = GameObject.Find("btn_Creer");
+		chkSynta = GameObject.Find ("chck_Syntaxique");
+		chkAction = GameObject.Find ("chck_Actions");
+
+		/*if (btnCreer.activeSelf == true) {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}else {
+			btnCreer.SetActive (false);
+			chkSynta.SetActive (false);
+			chkAction.SetActive (false);
+		}*/
+
+		//Deplacement des volet du dessus en haut de l'écran
+		recenterPos = rtNode.rect.position.y;
+		float anchorX = rtNode.anchoredPosition.x;
+		float anchorY = rtNode.anchoredPosition.y;
+
+		//1er volet du dessus
+		float anchorPosition = ((3*headerHeight)+(panelHeight-headerHeight)+(headerHeight/2))-windowHeight;
+		rtNode.anchoredPosition= new Vector2 (rtNode.anchoredPosition.x, anchorPosition);
+		anchorX = rtNode.anchoredPosition.x;
+		anchorY = rtNode.anchoredPosition.y;
+
+		//2eme volet en dessu
+		anchorX = rtActions.anchoredPosition.x;
+		anchorY = rtActions.anchoredPosition.y;
+		anchorPosition = -((headerHeight/2) + headerHeight);
+		rtActions.anchoredPosition = new Vector2 (rtActions.anchoredPosition.x, anchorPosition);
+		rtActions.sizeDelta =new Vector2(rtActions.rect.width, headerHeight);
+
+		//3eme volet au dessu
+		anchorX = rtParams.anchoredPosition.x;
+		anchorY = rtParams.anchoredPosition.y;
+		anchorPosition = -((headerHeight / 2) + headerHeight*2);
+		rtParams.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtParams.sizeDelta = new Vector2(rtParams.rect.width, headerHeight);
+
+		//Ouverture du panel cliqué
+		anchorX = rtOpe.anchoredPosition.x;
+		anchorY = rtOpe.anchoredPosition.y;
+		anchorPosition = -(3*headerHeight) - panelHeight / 2;
+		rtOpe.anchoredPosition = new Vector2 (rtParams.anchoredPosition.x, anchorPosition);
+		rtOpe.sizeDelta =new Vector2(rtParams.rect.width, panelHeight);
 	}
 
 }
-
