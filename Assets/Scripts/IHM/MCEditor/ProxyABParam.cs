a9ac4c61ff7f07ce4517190f873b3b0aa8481b03
galliotgreg <galliotgregpro@gmail.com>
2018-02-16 16:06:44 +0100

    Merge ProxyABParam

--- Assets/Scripts/IHM/MCEditor/ProxyABParam.cs
+++ Assets/Scripts/IHM/MCEditor/ProxyABParam.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProxyABParam<T> : MonoBehaviour, IProxyABParam{
 
-    private ABParam<T> abParam;
-    private Pin outcome;
     private bool isLoaded = false;
 
     public string Identifier {
@@ -48,8 +45,6 @@ public class ProxyABParam<T> : MonoBehaviour, IProxyABParam{
 
     // Use this for initialization
     void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
