85a985ae838f7de297eefad1233c09c9c1eb3a00
Tanguy Chanal <tanguy.chanal@gmail.com>
2018-02-21 16:14:02 +0100

    Rapprochement du panneau d'arrière-plan

---
+++ Assets/Scripts/IHM/Navigation/BringPanelForward.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BringPanelForward : MonoBehaviour {
+
+    public string sourcePanelName;
+    public string targetPanelName;
+
+    public float zoomStep;
+
+    // Use this for initialization
+    void Start () {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(MovePanel);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void MovePanel()
+    {
+        StartCoroutine(MovePanelCo());
+    }
+
+    IEnumerator MovePanelCo()
+    {
+        GameObject sourcePanel = GameObject.Find(sourcePanelName);
+        GameObject targetPanel = GameObject.Find(targetPanelName);
+        Vector3 sourcePanelPos = sourcePanel.transform.position;
+
+        Camera camera = GameObject.Find("NavigationManager").GetComponent<NavigationManager>().GetCurrentCamera();
+        //Camera camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        Vector3 dir = (sourcePanel.transform.position - camera.transform.position).normalized;
+
+        while (Vector3.Dot(dir, sourcePanel.transform.position - camera.transform.position) > 0)
+        {
+            Vector3 towards = Vector3.MoveTowards(
+                sourcePanel.transform.position,
+                camera.transform.position + sourcePanel.transform.position,
+                zoomStep);
+            sourcePanel.transform.position = towards;
+            yield return true;
+        }
+
+        Vector3 dir2 = (targetPanel.transform.position - sourcePanelPos).normalized;
+
+        while (Vector3.Dot(dir2, targetPanel.transform.position - sourcePanelPos) > 0)
+        {
+            Vector3 towards = Vector3.MoveTowards(
+                targetPanel.transform.position,
+                sourcePanelPos,
+                zoomStep);
+            targetPanel.transform.position = towards;
+            yield return true;
+        }
+
+        yield return true;
+    }
+}
