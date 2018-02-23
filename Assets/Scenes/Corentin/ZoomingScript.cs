782c406bed4ee6eec2f709433a7d9c5c7e5ccb03
Unknown <corentin.belloir@gmail.com>
2018-02-23 10:13:20 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scenes/Corentin/ZoomingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomingScript : MonoBehaviour {
+
+    private float minFov = 15f;
+    private float maxFov = 90f;
+    private float sensitivity = 10f;
+    private bool isZoomable = false;
+
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isZoomable = !isZoomable;
+        }
+
+        if (isZoomable)
+        {*/
+            float fov = Camera.main.fieldOfView;
+            fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            Camera.main.fieldOfView = fov;
+        //}
+    }
+}
