2789012ef40e6bb26c63da560684de5d3ba433d7
Narlugo <narutogoku2@hotmail.fr>
2018-02-26 15:41:06 +0100

    adding nuee

--- Assets/Scripts/ABProcessing/SyntaxTree/Operators/AB_ScalTab_Get_RefTab_Txt_Operator.cs
+++ Assets/Scripts/ABProcessing/SyntaxTree/Operators/AB_ScalTab_Get_RefTab_Txt_Operator.cs
@@ -2,11 +2,29 @@
 {
     public AB_ScalTab_Get_RefTab_Txt_Operator()
     {
-        throw new System.NotImplementedException();
+
+        this.Inputs = new ABNode[2];
+        //throw new System.NotImplementedException();
     }
 
     public override ABTable<ABScalar> Evaluate(ABContext context)
     {
-        throw new System.NotImplementedException();
+        ABTable<ABRef> tab = null;
+        ABNode input1 = Inputs[0];
+        tab = OperatorHelper.Instance.getTabRefParam(context, input1);
+
+        ABText t = null;
+        ABNode input2 = Inputs[1];
+        t = OperatorHelper.Instance.getTextParam(context, input2);
+
+        //Build then return Result
+        ABScalar[] values = new ABScalar[tab.Values.Length];
+        for (int i = 0; i < tab.Values.Length; i++) {
+            values[i] = (ABScalar)tab.Values[i].GetAttr(t.Value);
+        }
+        ABTable<ABScalar> result = TypeFactory.CreateEmptyTable<ABScalar>();
+        result.Values = values;
+        return result;
+        //throw new System.NotImplementedException();
     }
 }
