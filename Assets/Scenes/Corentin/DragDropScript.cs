ca392f074fc27256716c5a2b7e9cc5881f46b1b4
CorentinBelloir <corentin.belloir@gmail.com>
2018-02-28 15:12:34 +0100

    Integration navig IHM

--- Assets/Scenes/Corentin/DragDropScript.cs
+++ Assets/Scenes/Corentin/DragDropScript.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragDropScript : MonoBehaviour {
+public class DragDropScript : MonoBehaviour
+{
 
     GameObject getTarget;
     bool isMouseDragging;
-    //Vector3 offsetValue;
+    Vector3 offsetValue;
     Vector3 positionScreen;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
         //Mouse Button Press Down
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hitInfo;
             getTarget = ReturnClickedObject(out hitInfo);
-            if (getTarget != null)
+            if (getTarget != null && getTarget.name != "pin(Clone)")
             {
                 isMouseDragging = true;
                 Debug.Log(getTarget);
@@ -32,32 +35,33 @@ public class DragDropScript : MonoBehaviour {
         }
 
         //Mouse Button Up
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             isMouseDragging = false;
         }
 
         //Mouse moving
-        if(isMouseDragging)
+        if (isMouseDragging)
         {
             Debug.Log("isMouseDragging = " + isMouseDragging);
             //tracking mouse pos
-            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -GameObject.Find("Main Camera").GetComponent<Camera>().transform.position.z);
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -GameObject.Find("Camera").GetComponent<Camera>().transform.position.z);
 
             //converting screen pos => world pos with offset.
-            Vector3 currentPosition = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenToWorldPoint(currentScreenSpace); // + offsetValue;
+            Vector3 currentPosition = GameObject.Find("Camera").GetComponent<Camera>().ScreenToWorldPoint(currentScreenSpace);// + offsetValue;
 
             //update target current postion.
             getTarget.transform.position = currentPosition;
             Debug.Log(getTarget.transform.position);
         }
-	}
+    }
 
     //Method to Return Clicked Object
     GameObject ReturnClickedObject(out RaycastHit hit)
     {
         GameObject target = null;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = GameObject.Find("Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
         {
             target = hit.collider.gameObject;
