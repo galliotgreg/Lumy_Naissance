5d9d1eddbb6fd4c92b9d6e546a98f78d901bc2f0
Narlugo <narutogoku2@hotmail.fr>
2018-03-07 10:16:53 +0100

    #327 creation du debug manager
    creation du debug manager et mise a jour du prefab du lumy

---
+++ Assets/Scripts/Managers/DEBUG_Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_Manager : MonoBehaviour {
+
+    /// <summary>
+    /// The static instance of the Singleton for external access
+    /// </summary>
+    public static DEBUG_Manager instance = null;
+
+    public bool debug = false;
+
+    /// <summary>
+    /// Enforce Singleton properties
+    /// </summary>
+    void Awake() {
+        //Check if instance already exists and set it to this if not
+        if (instance == null) {
+            instance = this;
+        }
+
+        //Enforce the unicity of the Singleton
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Stubs/DEBUG_SetLumyRange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_SetLumyRange : MonoBehaviour {
+
+    // Use this for initialization
+    private float atkRange;
+    private float pickRange;
+    private float visionRange;
+    void Start () {
+
+        if(DEBUG_Manager.instance.debug == true) {
+
+            gameObject.SetActive(true);
+
+            GameObject go = gameObject;
+            atkRange = GetComponentInParent<AgentScript>().AtkRange;
+            pickRange = GetComponentInParent<AgentScript>().PickRange;
+            visionRange = GetComponentInParent<AgentScript>().VisionRange;
+
+            GameObject[] atkRangeObject = GameObject.FindGameObjectsWithTag("atkRange");
+            GameObject[] pickRangeObject = GameObject.FindGameObjectsWithTag("pickRange");
+            GameObject[] visionRangeObject = GameObject.FindGameObjectsWithTag("visionRange");
+
+            foreach (GameObject atkObj in atkRangeObject) {
+                atkObj.transform.localScale = new Vector3(atkRange * 2, 0.01f, atkRange * 2);
+            }
+
+            foreach (GameObject pickObj in pickRangeObject) {
+                pickObj.transform.localScale = new Vector3(pickRange * 2, 0.01f, pickRange * 2);
+            }
+
+            foreach (GameObject visionObj in visionRangeObject) {
+                visionObj.transform.localScale = new Vector3(visionRange * 2, 0.01f, visionRange * 2);
+            }
+        }
+        else {
+            gameObject.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
