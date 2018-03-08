7c442d48b2339d89b31a637ebad710bde3158c6d
Unknown <edwynluis@yahoo.com.br>
2018-03-08 13:54:16 +0100

    [#358] [bug fix] mceditor : save params values

--- Assets/Scripts/ABProcessing/SyntaxTree/Params/ABColorParam.cs
+++ Assets/Scripts/ABProcessing/SyntaxTree/Params/ABColorParam.cs
@@ -9,4 +9,16 @@ public class ABColorParam : ABParam<ABColor>
         this.identifier = identifier;
         this.value = value;
     }
+
+	#region implemented abstract members of ABParam
+
+	protected override IABParam CloneParam ()
+	{
+		int red = this.value.Value == ABColor.Color.Red ? 1 : 0;
+		int green = this.value.Value == ABColor.Color.Green ? 1 : 0;
+		int blue = this.value.Value == ABColor.Color.Blue ? 1 : 0;
+		return ABParamFactory.CreateColorParam ( this.identifier, red, green, blue );
+	}
+
+	#endregion
 }
--- Assets/Scripts/ABProcessing/SyntaxTree/Params/ABTableParam.cs
+++ Assets/Scripts/ABProcessing/SyntaxTree/Params/ABTableParam.cs
@@ -7,4 +7,19 @@ public class ABTableParam<T> : ABParam<ABTable<T>> where T : IABSimpleType {
         this.identifier = identifier;
         this.value = value;
     }
+
+	#region implemented abstract members of ABParam
+
+	protected override IABParam CloneParam ()
+	{
+		//throw new System.NotImplementedException ();
+		ABTable<T> table = new ABTable<T>();
+		table.Values = new T[this.value.Values.Length];
+		for (int i = 0; i < this.value.Values.Length; i++) {
+			table.Values [i] = ((T)this.value.Values.Clone ());
+		}
+		return new ABTableParam<T> (this.identifier, table);
+	}
+
+	#endregion
 }
