136c1d42d6797911763557e48f58ba70598f21b7
J-Ponzo <jeremy.ponzo@gmail.com>
2018-03-09 17:09:21 +0100

    add projectil prefab in order to fix strike

--- Assets/Scripts/View/ImportController.cs
+++ Assets/Scripts/View/ImportController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
 using System.IO;
 using Zip_Tool;
 
@@ -20,23 +19,26 @@ public class ImportController : MonoBehaviour {
     {
         string pol = @"C:/Users/etu2017/Desktop/MaNuee7";
         string ol = @"C:/Users/etu2017/Desktop/TMP/test/Lumy_Naissance/Assets/Inputs/Species";
-        FileUtil.CopyFileOrDirectory(pol, ol);
+        //FileUtil.CopyFileOrDirectory(pol, ol);
     }
 
     void ImportSpecie()
     {
+        Debug.LogError("Import not implemented yet !");
+
         // System.Diagnostics.Process.Start("Explorer.exe", @"/select,""" + filePath);
         //System.Diagnostics.Process.Start("explorer.exe", string.Format("/select,\"{0}\"", filePath));
         //string defaultName = BuildDefaultName();
         //AppContextManager.instance.CreateSpecie(defaultName);
-        string path = EditorUtility.OpenFilePanel("Name", filePath, "");
-        if (path != null){
-            Debug.Log(path);
-            string[] path_cutted = path.Split('/');
-            string name = path_cutted[path_cutted.Length - 1];
-            //Decompress(path);
-            ZipUtil.Unzip(path, filePath);
-        }
+
+        //string path = EditorUtility.OpenFilePanel("Name", filePath, "");
+        //if (path != null){
+        //    Debug.Log(path);
+        //    string[] path_cutted = path.Split('/');
+        //    string name = path_cutted[path_cutted.Length - 1];
+        //    //Decompress(path);
+        //    ZipUtil.Unzip(path, filePath);
+        //}
     }
 
 }
