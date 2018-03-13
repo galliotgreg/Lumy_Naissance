47af7fc4395ed3bdc89680eb192c987c0f2dabdb
Unknown <thomasousselin0902@gmail.com>
2018-03-13 16:11:31 +0100

    #377 lisibilité match

--- Assets/Scripts/Stubs/DEBUG_mineraisStock.cs
+++ Assets/Scripts/Stubs/DEBUG_mineraisStock.cs
@@ -5,25 +5,58 @@ using UnityEngine.UI;
 
 public class DEBUG_mineraisStock : MonoBehaviour {
 
+
+    GameObject[] minerais = null;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 
     // Update is called once per frame
     void Update() {
 
-        if (DEBUG_Manager.instance.debugMineraiStock == true) {
+       /* if (DEBUG_Manager.instance.debugMineraiStock == true) {
 
-            gameObject.SetActive(true);
+            gameObject.GetComponentInChildren<Text>().gameObject.SetActive(true);
 
             string stock = GetComponentInParent<ResourceScript>().Stock.ToString();
             Text stockText = gameObject.GetComponentInChildren<Text>();
             stockText.text = stock;
         }
-        else {
-            gameObject.SetActive(false);
+        else
+        {
+            gameObject.GetComponentInChildren<Text>().gameObject.SetActive(false);
+        }*/
+
+        if(OptionManager.instance == null)
+        {
+            return;
         }
+
+
+        if (OptionManager.instance.gisementsBool == true)
+        {
+
+            minerais = OptionManager.instance.GetMinerals();
+            foreach(GameObject ressource in minerais)
+            {
+                ressource.GetComponentInChildren<Canvas>().gameObject.SetActive(true);
+
+            }
+        }
+        else
+        {
+            Debug.Log("FAUX");
+            minerais = OptionManager.instance.GetMinerals();
+            foreach (GameObject ressource in minerais)
+            {
+                ressource.GetComponentInChildren<Canvas>().gameObject.SetActive(false);
+
+            }
+        }
+
     }
+
     
 }
