b831d8f2a4a64857b276d4a4f4c9d482f2a16afe
Tanguy Chanal <tanguy.chanal@gmail.com>
2018-03-15 17:37:12 +0100

    [#404] Scène de vidéo d'introduction

---
+++ Assets/Scripts/Video/PlayThenLoadVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayThenLoadVideo : MonoBehaviour {
+
+    public string target;
+    public GameObject player;
+    public Button skip;
+
+    // Use this for initialization
+    void Start () {
+        player.GetComponent<VideoPlayer>().Play();
+        StartCoroutine(WaitForLengthCo(player.GetComponent<VideoPlayer>().clip.length));
+        skip.onClick.AddListener(SkipVideo);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator WaitForLengthCo(double duration)
+    {
+        yield return new WaitForSeconds((float)duration);
+        NavigationManager.instance.ActivateFadeToBlack();
+        NavigationManager.instance.SwapScenesWithoutZoom(target);
+        player.GetComponent<VideoPlayer>().Stop();
+    }
+
+    void SkipVideo()
+    {
+        NavigationManager.instance.ActivateFadeToBlack();
+        NavigationManager.instance.SwapScenesWithoutZoom(target);
+        StopCoroutine(WaitForLengthCo(player.GetComponent<VideoPlayer>().clip.length));
+    }
+}
