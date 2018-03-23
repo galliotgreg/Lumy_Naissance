d91248abece09c5a51e00e0cc3f6217ecdd678aa
Michel <garcia-l.michel@hotmail.fr>
2018-03-23 16:54:24 +0100

    BIM y a de la musique : PlaceHolder pour les menus

---
+++ Assets/Scripts/Musique_SFX/Musique/IntroThenLoop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class IntroThenLoop : MonoBehaviour
+{
+    public AudioClip StartClip;
+    public AudioClip LoopClip;
+
+    void Start()
+    {
+        StartCoroutine(playSound());
+    }
+
+    IEnumerator playSound()
+    {
+        GetComponent<AudioSource>().clip = StartClip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(StartClip.length);
+        GetComponent<AudioSource>().clip = LoopClip;
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().loop = true;
+    }
+}
