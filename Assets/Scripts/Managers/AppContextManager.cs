ff95e53539ee88c1456f99679b3a5a8ba1f21f5f
J-Ponzo <jeremy.ponzo@gmail.com>
2018-03-26 13:55:02 +0200

    #488 Impossible de renomer un Lumy

--- Assets/Scripts/Managers/AppContextManager.cs
+++ Assets/Scripts/Managers/AppContextManager.cs
@@ -89,6 +89,12 @@ public class AppContextManager : MonoBehaviour
     public string CAST_FILES_SUFFIX = "_behavior";
 
     /// <summary>
+    /// Casts positions files suffix
+    /// </summary>
+    [SerializeField]
+    public string POSITION_FILES_SUFFIX = "_behavior_POSITION";
+
+    /// <summary>
     /// Casts files suffix
     /// </summary>
     [SerializeField]
@@ -393,8 +399,13 @@ public class AppContextManager : MonoBehaviour
         File.AppendAllText(activeSpecieFilePath, content);
     }
 
-    public void SaveCast()
+    public void SaveCast(Cast trgCast)
     {
+        if (trgCast == null)
+        {
+            trgCast = activeCast;
+        }
+
         //Read old file
         StreamReader reader = new StreamReader(activeSpecieFilePath);
         List<string> lines = new List<string>();
@@ -409,7 +420,7 @@ public class AppContextManager : MonoBehaviour
         foreach (string line in lines)
         {
             string[] tokens = line.Split(',');
-            if (tokens[0] == activeCast.Name && tokens[1] == activeCast.BehaviorModelIdentifier)
+            if (tokens[0] == trgCast.Name && tokens[1] == trgCast.BehaviorModelIdentifier)
             {
                 content += activeCast.Name + "," + activeCast.BehaviorModelIdentifier + "," + activeCast.Head.Count + ",";
                 foreach (ComponentInfo compoInfo in activeCast.Head)
