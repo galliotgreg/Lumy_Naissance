8dc7dabcb9675cc047b7b7ea9faef559b996891e
Unknown <edwynluis@yahoo.com.br>
2018-03-28 12:25:53 +0200

    [#516] [bug fix] fill actions params

--- Assets/Scripts/GameObject/AgentBehavior.cs
+++ Assets/Scripts/GameObject/AgentBehavior.cs
@@ -97,6 +97,7 @@ public class AgentBehavior : MonoBehaviour
 
 		GameAction currentGameAction = getGameAction (this.curAction);
 		if (currentGameAction != null) {
+			fillActionParams ();
 			currentGameAction.frameBegin ();
 		}
 	}
@@ -116,7 +117,6 @@ public class AgentBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		executeAction();
     }
 
     private void FrameEnd()
@@ -130,108 +130,98 @@ public class AgentBehavior : MonoBehaviour
 		roamingAction.frameEnd ();
     }
 
-	private void executeAction(){
-		try{
-
-		//We arre between 2 IA frames
-		/*if (curActionParams == null)
-		{
-			return;
-		}*/
-		// no Action
-		if (curAction == null)
-		{
-			return;
-		}
-
-		// Inject Param on corresponding Action Script then enable it
-		switch (curActionType)
-		{
-		case ActionType.Drop:
-			dropAction.activate();
-			break;
-		case ActionType.Goto:
-			ABTable<ABVec> path = ((ABTable<ABVec>)curActionParams [0]);
-			gotoAction.Path = new Vector3[path.Values.Length];
-			for (int i = 0; i < path.Values.Length; i++)
-			{
-				ABVec abVec = path.Values[i];
-				Vector3 vec3 = vec2ToWorld( new Vector2( abVec.X, abVec.Y ) );
-				gotoAction.Path[i] = vec3;
-			}
-			
-			gotoAction.activate();
-			break;
-		case ActionType.Hit:
-			throw new System.NotImplementedException();
-			break;
-		case ActionType.Hold:
-			throw new System.NotImplementedException();
-			break;
-		case ActionType.Lay:
-			ABText castName = ((ABText)curActionParams[0]);
-			layAction.CastName = castName.Value;
-			
-			layAction.activate();
-			break;
-		case ActionType.Pick:
-			ABRef item = ((ABRef)curActionParams[0]);
-			pickAction.Item = Unit_GameObj_Manager.instance.getResource( Mathf.FloorToInt( ((ABScalar)item.GetAttr( "key" )).Value ) );
-
-			pickAction.activate();
-			break;
-		case ActionType.Spread:
-			throw new System.NotImplementedException();
-			break;
-		case ActionType.Trace:
-			// color
-			ABColor traceColor = ((ABColor)CurActionParams[0]);
-			switch( traceColor.Value ){
-				case ABColor.Color.Red:
-					traceAction.Color = Color.red;
+	private void fillActionParams(){
+		if (curAction != null) {
+			try{
+				// Inject Param on corresponding Action Script then enable it
+				switch (curAction.Type)
+				{
+				case ActionType.Drop:
+					dropAction.activate();
 					break;
-				case ABColor.Color.Green:
-					traceAction.Color = Color.green;
+				case ActionType.Goto:
+					ABTable<ABVec> path = ((ABTable<ABVec>)curActionParams [0]);
+					gotoAction.Path = new Vector3[path.Values.Length];
+					for (int i = 0; i < path.Values.Length; i++)
+					{
+						ABVec abVec = path.Values[i];
+						Vector3 vec3 = vec2ToWorld( new Vector2( abVec.X, abVec.Y ) );
+						gotoAction.Path[i] = vec3;
+					}
+
+					gotoAction.activate();
 					break;
-				case ABColor.Color.Blue:
-					traceAction.Color = Color.blue;
+				case ActionType.Hit:
+					throw new System.NotImplementedException();
 					break;
+				case ActionType.Hold:
+					throw new System.NotImplementedException();
+					break;
+				case ActionType.Lay:
+					ABText castName = ((ABText)curActionParams[0]);
+					layAction.CastName = castName.Value;
+
+					layAction.activate();
+					break;
+				case ActionType.Pick:
+					ABRef item = ((ABRef)curActionParams[0]);
+					pickAction.Item = Unit_GameObj_Manager.instance.getResource( Mathf.FloorToInt( ((ABScalar)item.GetAttr( "key" )).Value ) );
+
+					pickAction.activate();
+					break;
+				case ActionType.Spread:
+					throw new System.NotImplementedException();
+					break;
+				case ActionType.Trace:
+					// color
+					ABColor traceColor = ((ABColor)CurActionParams[0]);
+					switch( traceColor.Value ){
+					case ABColor.Color.Red:
+						traceAction.Color = Color.red;
+						break;
+					case ABColor.Color.Green:
+						traceAction.Color = Color.green;
+						break;
+					case ABColor.Color.Blue:
+						traceAction.Color = Color.blue;
+						break;
+					}
+
+					// path
+					ABTable<ABVec> tracePath = ((ABTable<ABVec>)curActionParams[1]);
+					traceAction.Path = new Vector3[tracePath.Values.Length];
+					for (int i = 0; i < tracePath.Values.Length; i++)
+					{
+						ABVec abVec = tracePath.Values[i];
+						Vector3 vec3 = vec2ToWorld( new Vector2( abVec.X, abVec.Y ) );
+						traceAction.Path[i] = vec3;
+					}
+
+					traceAction.activate();
+					break;
+				case ActionType.Strike:
+					ABRef target = ((ABRef)curActionParams [0]);
+					if (target != null) {
+						strikeAction.Target = Unit_GameObj_Manager.instance.getUnit (Mathf.FloorToInt (((ABScalar)target.GetAttr ("key")).Value));
+					}
+
+					strikeAction.activate();
+					break;
+				case ActionType.Roaming:
+					ABScalar angle = ((ABScalar)curActionParams [0]);
+					ABScalar dist = ((ABScalar)curActionParams [1]);
+					roamingAction.setParams( angle.Value, dist.Value );
+
+					roamingAction.activate();
+					break;
+				case ActionType.None:
+					break;
+				}
+
 			}
-
-			// path
-			ABTable<ABVec> tracePath = ((ABTable<ABVec>)curActionParams[1]);
-			traceAction.Path = new Vector3[tracePath.Values.Length];
-			for (int i = 0; i < tracePath.Values.Length; i++)
-			{
-				ABVec abVec = tracePath.Values[i];
-				Vector3 vec3 = vec2ToWorld( new Vector2( abVec.X, abVec.Y ) );
-				traceAction.Path[i] = vec3;
+			catch( System.Exception ex ){
+				throw new Action_Exception ( curAction, null, ex.Message );
 			}
-
-			traceAction.activate();
-			break;
-		case ActionType.Strike:
-			ABRef target = ((ABRef)curActionParams [0]);
-			if (target != null) {
-				strikeAction.Target = Unit_GameObj_Manager.instance.getUnit (Mathf.FloorToInt (((ABScalar)target.GetAttr ("key")).Value));
-			}
-
-			strikeAction.activate();
-			break;
-		case ActionType.Roaming:
-				ABScalar angle = ((ABScalar)curActionParams [0]);
-				ABScalar dist = ((ABScalar)curActionParams [1]);
-				roamingAction.setParams( angle.Value, dist.Value );
-
-				roamingAction.activate();
-			break;
-		case ActionType.None:
-			break;
-		}
-		
-		}
-		catch( System.Exception ex ){
-			throw new Action_Exception ( curAction, null, ex.Message );
 		}
 	}
 
