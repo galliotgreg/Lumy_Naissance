e7281595974b5dedb5c5159ef696770a0f0a76cd
Michel <garcia-l.michel@hotmail.fr>
2018-04-07 10:42:37 +0200

    duplication de Ingamescene pour tests

---
+++ Assets/Scripts/IHM/InGame/OpenClosePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenClosePanel : MonoBehaviour {
+
+    public GameObject panelStats;
+    public Button btn_panelStats;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
