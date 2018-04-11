a54eb7cad9c1e31f610c82a7967b662ea37375b0
Charles <grusscharles@gmail.com>
2018-04-11 17:54:32 +0200

    #618 - Pictos Affrontement

--- Assets/Scripts/IHM/PartiePerso/PartiePersoUIController.cs
+++ Assets/Scripts/IHM/PartiePerso/PartiePersoUIController.cs
@@ -465,7 +465,27 @@ public class PartiePersoUIController : MonoBehaviour {
         
         //Compute and display stats
         ComputePlayer1Stats();
-      
+
+        DisplayPictoNueeP1();
+    }
+
+    private void DisplayPictoNueeP1()
+    {
+        GameObject[] swarmPictos = LumyPictFactory.instance.InstanciateAllPicts();
+
+        // Get prefab Texture
+        Material mat = swarmPictos[AppContextManager.instance.ActiveSpecie.PictId].GetComponent<MeshRenderer>().material;
+        Texture2D texturPicto = (Texture2D)mat.mainTexture;
+        Sprite spritePicto = Sprite.Create(texturPicto, new Rect(0.0f, 0.0f, texturPicto.width, texturPicto.height), new Vector2(0.5f, 0.5f), 100.0f);
+        //Display Swarm Picto
+        pictoSwarmP1.gameObject.SetActive(true);
+        pictoSwarmP1.sprite = spritePicto;
+
+        foreach (GameObject picto in swarmPictos)
+        {
+            Destroy(picto);
+        }
+
     }
 
     private void ClearPlayer1View()
@@ -511,19 +531,26 @@ public class PartiePersoUIController : MonoBehaviour {
         //Compute and display stats
         ComputePlayer2Stats();
 
-        //DisplayPictoNueeP2();
+        DisplayPictoNueeP2();
  
     }
 
     private void DisplayPictoNueeP2()
     {
         GameObject[] swarmPictos = LumyPictFactory.instance.InstanciateAllPicts();
-        // Get prefab material
+        
+        // Get prefab Texture
         Material mat = swarmPictos[AppContextManager.instance.ActiveSpecie.PictId].GetComponent<MeshRenderer>().material;
         Texture2D texturPicto = (Texture2D) mat.mainTexture;
         Sprite spritePicto = Sprite.Create(texturPicto, new Rect(0.0f, 0.0f, texturPicto.width, texturPicto.height), new Vector2(0.5f, 0.5f), 100.0f);
-
+        //Display Swarm Picto
+        pictoSwarmP2.gameObject.SetActive(true);
         pictoSwarmP2.sprite = spritePicto;
+
+        foreach(GameObject picto in swarmPictos)
+        {
+            Destroy(picto);
+        }
 
     }
 
