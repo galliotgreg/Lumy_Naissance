c8149b8f9fe58639b03e588ba042d0a27b025931
Charles <grusscharles@gmail.com>
2018-04-11 18:04:53 +0200

    #618 - Correction Pictos Affrontement

--- Assets/Scripts/IHM/PartiePerso/PartiePersoUIController.cs
+++ Assets/Scripts/IHM/PartiePerso/PartiePersoUIController.cs
@@ -474,8 +474,8 @@ public class PartiePersoUIController : MonoBehaviour {
         GameObject[] swarmPictos = LumyPictFactory.instance.InstanciateAllPicts();
 
         // Get prefab Texture
-        Material mat = swarmPictos[AppContextManager.instance.ActiveSpecie.PictId].GetComponent<MeshRenderer>().material;
-        Texture2D texturPicto = (Texture2D)mat.mainTexture;
+        Material mat = swarmPictos[player1Specie.PictId].GetComponent<MeshRenderer>().material;
+        Texture2D texturPicto = (Texture2D) mat.mainTexture;
         Sprite spritePicto = Sprite.Create(texturPicto, new Rect(0.0f, 0.0f, texturPicto.width, texturPicto.height), new Vector2(0.5f, 0.5f), 100.0f);
         //Display Swarm Picto
         pictoSwarmP1.gameObject.SetActive(true);
@@ -540,7 +540,7 @@ public class PartiePersoUIController : MonoBehaviour {
         GameObject[] swarmPictos = LumyPictFactory.instance.InstanciateAllPicts();
         
         // Get prefab Texture
-        Material mat = swarmPictos[AppContextManager.instance.ActiveSpecie.PictId].GetComponent<MeshRenderer>().material;
+        Material mat = swarmPictos[player2Specie.PictId].GetComponent<MeshRenderer>().material;
         Texture2D texturPicto = (Texture2D) mat.mainTexture;
         Sprite spritePicto = Sprite.Create(texturPicto, new Rect(0.0f, 0.0f, texturPicto.width, texturPicto.height), new Vector2(0.5f, 0.5f), 100.0f);
         //Display Swarm Picto
