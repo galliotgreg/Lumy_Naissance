8a33323c035d006870c3c4fea960507fedc89da5
Michel <garcia-l.michel@hotmail.fr>
2018-04-20 17:37:25 +0200

    New SFX Play/Pause

---
+++ Assets/Scripts/Musique_SFX/SFX/ButtonInGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInGame : MonoBehaviour {
+
+    public Button menuDeroulant;
+
+    public Button menuPrincipal;
+    public Button affrontement;
+    public Button nuee;
+    public Button options;
+
+    public Button slideContexte;
+    public Button slideStats;
+    public Button slideMC;
+
+	void Start () {
+        menuDeroulant.onClick.AddListener(SoundManager.instance.PlayOnClickButtonSFX);
+
+    }
+	
+}
