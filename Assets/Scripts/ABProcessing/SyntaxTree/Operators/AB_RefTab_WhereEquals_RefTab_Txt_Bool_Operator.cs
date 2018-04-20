bc13e19dc8c7cf41234b01a9da5dd58a2b28eb7b
Narlugo <narutogoku2@hotmail.fr>
2018-04-20 10:57:25 +0200

    modification des operateurs where

--- Assets/Scripts/ABProcessing/SyntaxTree/Operators/AB_RefTab_WhereEquals_RefTab_Txt_Bool_Operator.cs
+++ Assets/Scripts/ABProcessing/SyntaxTree/Operators/AB_RefTab_WhereEquals_RefTab_Txt_Bool_Operator.cs
@@ -1,4 +1,6 @@
-﻿public class AB_RefTab_WhereEquals_RefTab_Txt_Bool_Operator : ABOperator<ABTable<ABRef>>
+﻿using System.Collections.Generic;
+
+public class AB_RefTab_WhereEquals_RefTab_Txt_Bool_Operator : ABOperator<ABTable<ABRef>>
 {
     public AB_RefTab_WhereEquals_RefTab_Txt_Bool_Operator()
     {
@@ -25,12 +27,19 @@
 
         //Return
         ABTable<ABRef> result = TypeFactory.CreateEmptyTable<ABRef>();
+        
+        IList<ABRef> listResult = new List<ABRef>();
 
-        for(int i = 0; i < tab.Values.Length; i++) {
 
-            if (tab.Values[i].GetAttr(text.Value) != null && ((ABBool) tab.Values[i].GetAttr(text.Value)).Value == b1.Value) {
-                result.Values[i] = tab.Values[i];
+        for (int i = 0; i < tab.Values.Length; i++) {
+
+            if (tab.Values[i].GetAttr(text.Value) != null && ((ABBool)tab.Values[i].GetAttr(text.Value)).Value == b1.Value) {
+                listResult.Add(tab.Values[i]);
             }
+        }
+        result.Values = new ABRef[listResult.Count];
+        for (int i = 0; i < listResult.Count; i++) {
+            result.Values[i] = listResult[i];
         }
 
         return result;
